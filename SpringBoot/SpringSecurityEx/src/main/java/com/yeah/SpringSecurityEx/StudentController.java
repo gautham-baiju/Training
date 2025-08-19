95a0b9a4984b24d91a4c7901008eaa8f3afc210a
Gautham Baiju <gautham.baiju@vonnue.com>
2025-08-19 09:03:11 +0530

    created sample application for spring security

---
+++ SpringBoot/SpringSecurityEx/src/main/java/com/yeah/SpringSecurityEx/StudentController.java
@@ -0,0 +1,39 @@
+package com.yeah.SpringSecurityEx;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "apple", 123),
+            new Student(2, "mango", 333)));
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @PostMapping("/student")
+    public Student getStudents(@RequestBody Student student) {
+        students.add(student);
+        return student;
+    }
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
